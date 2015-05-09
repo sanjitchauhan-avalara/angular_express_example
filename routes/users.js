7a59f2ce99bc84f2b08b6e22e49c7279d081a726
pramod-rathod-avalara <pramod.rathod@avalara.com>
2015-05-09 10:41:10 +0530

    Usage of middleware using next().

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,17 @@
 var express = require('express');
 var router = express.Router();
 
+var counter = 1;
+
 /* GET users listing. */
-router.get('/', function(req, res) {
-  res.send('respond with a resource');
+router.get('/', 
+  function (req, res, next) {
+    counter++;
+    req.counter = counter;
+    next();
+  }
+  ,function(req, res, next) {
+  res.send('Counter value is ' + req.counter);
 });
 
 module.exports = router;
