99c3e4455db8f3aa2db070b29460eeb555c4d721
pramod-rathod-avalara <pramod.rathod@avalara.com>
2015-05-09 11:09:22 +0530

    Getting request param, dynamic url variables

--- routes/users.js
+++ routes/users.js
@@ -4,14 +4,24 @@ var router = express.Router();
 var counter = 1;
 
 /* GET users listing. */
-router.get('/', 
-  function (req, res, next) {
-    counter++;
-    req.counter = counter;
-    next();
-  }
-  ,function(req, res, next) {
-  res.send('Counter value is ' + req.counter);
+router.get('/',
+    function(req, res, next) {
+        counter++;
+        req.counter = counter;
+        next();
+    },
+    function(req, res, next) {
+        res.send('Counter value is ' + req.counter);
+    });
+
+
+router.post('/', function(req, res, next) {
+  res.send(req.body);
+});
+
+router.get('/:id', function(req, res, next) {
+  var id = req.param('id');
+  res.send('Request id is ' + id);
 });
 
 module.exports = router;
