ed2bab28f7f6e6d2e8d7f0033bd1f96a1bcf81d0
pramod-rathod-avalara <pramod.rathod@avalara.com>
2015-05-09 11:00:27 +0530

    Embedded js template variables.

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,17 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Express' });
+  var acceptLanguage = req.headers["accept-language"] ;
+  var languages = acceptLanguage.match(/[a-zA-z\-]{2,10}/g) || [];
+  var greeting = 'Svagata';
+  if(languages[0] == 'en-US') {
+    greeting = 'Welcome';
+  } else if ( languages[0] == 'hi') {
+    greeting = 'Svagata';
+  } else {
+    greeting = 'Svagata';
+  }
+  res.render('index', { greeting: greeting, title: 'Express' });
 });
 
 module.exports = router;
